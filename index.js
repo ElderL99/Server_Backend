4f86c39c2af73bffa27a04e003a88c3e88e5fb5a
ElderL99 <adan.lugo.barrientos@outlook.com>
2024-09-19 18:25:58 -0600

    cambios

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 const server = require("./server"); // Importa la instancia de Express
 const db = require("./src/lib/db"); // Asumo que es tu módulo de conexión a la base de datos
 
-const port = process.env.PORT || 10000; // Usa el puerto asignado por la plataforma o 10000 para desarrollo local
+
+const port = process.env.PORT || 10000;
 
 db.connect()
   .then(() => {
